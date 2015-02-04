c4044e4be844b951b7869277fa6fe4e4e03aab44
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-04 04:12:33 +0000

    j2objc 0.9.6
    Version bump.
    
    Once the bot is fixed, I won't commit directly to Master. If you
    catch me doing this post-bot-fix, yell at me.

--- Formula/j2objc.rb
+++ Formula/j2objc.rb
@@ -2,13 +2,13 @@ class J2objc < Formula
   homepage "http://j2objc.org/"
 
   devel do
-    url "https://github.com/google/j2objc/releases/download/0.9.5/j2objc-0.9.5.zip"
-    sha1 "6b53c2b47c9cd4c6678d0fee75b00039b83fc120"
+    url "https://github.com/google/j2objc/releases/download/0.9.6/j2objc-0.9.6.zip"
+    sha1 "02cd1e73331e0f759d38643c6f67cccdd55a9cbd"
   end
 
   depends_on :xcode
   depends_on :macos => :mavericks
-  depends_on :java => "1.7"
+  depends_on :java => "1.7+"
 
   def install
     # Building from source is a nightmare right now, so use the shipped binaries.
@@ -19,6 +19,8 @@ class J2objc < Formula
     bin.write_exec_script libexec/"cycle_finder"
     bin.write_exec_script libexec/"j2objc"
     bin.write_exec_script libexec/"j2objcc"
+    man1.install_symlink libexec/"man/man1/j2objc.1"
+    man1.install_symlink libexec/"man/man1/j2objcc.1"
   end
 
   test do
