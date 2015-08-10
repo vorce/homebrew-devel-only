5d525c7f9e0ab431c79f7f2ec2b693ef4741adf6
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-10 18:04:23 +0100

    shadowvpn 0.1.7 (devel)
    Closes #22.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/shadowvpn.rb
+++ Formula/shadowvpn.rb
@@ -1,15 +1,19 @@
 class Shadowvpn < Formula
+  desc "Fast, safe VPN based on libsodium"
   homepage "https://shadowvpn.org/"
 
   devel do
-    url "https://github.com/clowwindy/ShadowVPN/releases/download/0.1.6/shadowvpn-0.1.6.tar.gz"
-    sha256 "fd2b4a25185a2c6ae9570c4f1bbb2b06412fe54985e8c007119666ac12d5ff04"
+    url "https://github.com/clowwindy/ShadowVPN/releases/download/0.1.7/shadowvpn-0.1.7.tar.gz"
+    sha256 "e95d0cb7b4ce5489ec9392acb57af7cb4ccfe6f9dc87e0f2937006ec3cabfb03"
   end
 
   depends_on :tuntap
+  # Check the below again on the next release.
+  depends_on :macos => :mavericks
 
   def install
-    # Conflicts with libsodium, due to shipping internally. libexec it all.
+    # Conflicts with libsodium, due to shipping internally.
+    # Rather than conflict, install in libexec.
     system "./configure", "--prefix=#{libexec}", "--sysconfdir=#{etc}",
                           "--localstatedir=#{var}", "--disable-dependency-tracking"
     system "make", "install"
@@ -17,9 +21,6 @@ class Shadowvpn < Formula
   end
 
   def caveats; <<-EOS.undent
-    Running the executable currently requires sudo.
-    If you aren't root, you cannot use the binary at this point in time.
-
     You will also need to correct the tun references in #{etc}/shadowvpn/*.conf
     to fit the tun/tap interface on your system.
     EOS
@@ -27,6 +28,6 @@ class Shadowvpn < Formula
 
   test do
     output = shell_output("#{bin}/shadowvpn -h; true")
-    assert output.include?("usage: shadowvpn -c config_file [-s start/stop/restart]")
+    assert_match /usage: shadowvpn -c config_file/, output
   end
 end
