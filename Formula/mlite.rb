b8861d4ecd24c32b062ead594e44b157684787a3
Baptiste Fontaine <baptiste.fontaine@batch.com>
2015-08-27 11:08:03 +0200

    mlite: add description
    Closes #30.
    
    Signed-off-by: Baptiste Fontaine <baptiste.fontaine@batch.com>

--- Formula/mlite.rb
+++ Formula/mlite.rb
@@ -1,9 +1,10 @@
 class Mlite < Formula
+  desc "Lightweight ML-like language"
   homepage "http://t3x.org/mlite/index.html"
 
   devel do
     url "http://t3x.org/mlite/mlite-20141229.tgz"
-    sha1 "2c150bbbff33fa6bbd9aac7de00a374c129105f5"
+    sha256 "54c24e6703baded5076edefb206985a62ad28c6f9575ed5394cf82284934c65a"
   end
 
   def install
@@ -13,7 +14,7 @@ class Mlite < Formula
   end
 
   test do
-    (testpath/"test.m").write("len ` iota 1000")
+    (testpath/"test.m").write "len ` iota 1000"
     system "#{bin}/mlite", "-f", "test.m"
   end
 end
