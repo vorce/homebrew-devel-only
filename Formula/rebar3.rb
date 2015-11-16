b12f533b49c218db7abbb38800383d91c611d0d5
niku <niku@niku.name>
2015-11-16 12:33:30 +0900

    rebar3 beta-4
    Closes #37.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -6,8 +6,9 @@ class Rebar3 < Formula
     # We cannot use tar/zip file.
     # It needs git repository for versioning at bootstrap.
     url "https://github.com/rebar/rebar3.git",
-        :tag => "3.0.0-beta.3",
-        :revision => "4177c75a7c709b615962b1a885c119a51ed0aab0"
+        :tag => "beta-4",
+        :revision => "356ac5033d12b8b91ed0d6c4d308b00070ab12b9"
+    version "3.0.0-beta-4"
 
     depends_on "erlang"
   end
